eaca461a2ad7efa2ed94899fd67df3eb74c17579
Janek <jpotworowski@gmail.com>
2019-09-21 15:50:25 +0200

    refactored add actor and add director

--- project_imdb/imdb_manager/imdb_manager.py
+++ project_imdb/imdb_manager/imdb_manager.py
@@ -1,10 +1,11 @@
 import pymysql
 from project_imdb.config import host, user, password, db
-from project_imdb.imdb_manager.imdb_queries import add_actor_query, get_actor_id_query
+from project_imdb.imdb_manager.imdb_queries import add_actor_query, get_actor_id_query, add_person_query
 
-class Actor:
+
+class Person:
     """
-    Hods data about actor
+    Holds data about person
     """
     def __init__(self, first_name, last_name, nationality):
         self.first_name = first_name
@@ -21,16 +22,33 @@ class ImdbManager:
         """
         self.conn = pymysql.connect(host=host, user=user, password=password, charset='utf8', db=db)
 
-    def addActor(self, actor):
+    def addPerson(self, person, table):
+        """
+        adds person to  table given person object
+        :param person: person object
+        :param table: target table
+        :return:
+        """
+        cursor = self.conn.cursor()
+        print(add_person_query % (table, person.first_name, person.last_name, person.nationality))
+        cursor.execute(add_person_query % (table, person.first_name, person.last_name, person.nationality))
+        self.conn.commit()
+
+    def addActor(self, person):
         """
         adds actor to actor table given first actor object
         :param actor: actor object
         :return: id in actor table
         """
-        # create cursor
-        cursor = self.conn.cursor()
-        cursor.execute(add_actor_query % (actor.first_name, actor.last_name, actor.nationality))
-        self.conn.commit()
+        self.addPerson(person, 'actor')
+
+    def addDirector(self, person):
+        """
+        adds actor to actor table given first actor object
+        :param actor: actor object
+        :return: id in actor table
+        """
+        self.addPerson(person, 'director')
 
     def getActorId(self, actor):
         """
@@ -40,11 +58,12 @@ class ImdbManager:
         """
         # create cursor
         cursor = self.conn.cursor()
-        cursor.execute(get_actor_id_query %(actor.first_name, actor.last_name))
+        cursor.execute(get_actor_id_query % (actor.first_name, actor.last_name))
         return cursor.fetchall()[0][0]
 
 
 if __name__ == "__main__":
     imdb_manager = ImdbManager(host, user, password, db)
-    actor = Actor(first_name='Jerzy', last_name='Stuhr', nationality='PL')
-    print(imdb_manager.getActorId(actor))
+    person = Person(first_name='Jerzy', last_name='Stuhr', nationality='PL')
+    imdb_manager.addActor(person)
+    # print(imdb_manager.getActorId(actor))
--- project_imdb/imdb_manager/imdb_queries.py
+++ project_imdb/imdb_manager/imdb_queries.py
@@ -5,3 +5,11 @@ INSERT INTO actor (firstName, lastName, nationality) VALUES ('%s', '%s', '%s');
 get_actor_id_query = """
 select ID from actor where firstName='%s' and lastName='%s' order by ID limit 1;
 """
+
+add_director_query = """
+INSERT INTO director (firstName, lastName, nationality) VALUES ('%s', '%s', '%s');
+"""
+
+add_person_query = """
+INSERT INTO %s (firstName, lastName, nationality) VALUES ('%s', '%s', '%s');
+"""
