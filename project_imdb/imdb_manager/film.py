cbe983fe071e8f381a6e6378de6a70f748f67348
Janek <jpotworowski@gmail.com>
2019-09-22 10:02:10 +0200

    created film class, started work on addFilm method

---
+++ project_imdb/imdb_manager/film.py
@@ -0,0 +1,61 @@
+class Film:
+    """
+    Holds information about one film
+    """
+
+    def __init__(self,
+                 title,
+                 rel_year=None,
+                 duration=None,
+                 director=None,
+                 rating=None,
+                 voters=None,
+                 ranking=None,
+                 orig_title=None,
+                 actors=None,
+                 genres=None):
+        """
+        Film constructor
+        :param title: film title string
+        :param rel_year: release year - string  or int (converted to int)
+        :param duration: string with duration
+        :param actors: list of strings with actors names
+        :param director: string with directors name
+        :param rating: string with rating
+        :param ranking: string with ranking
+        :param voters: string with voters
+        :
+
+        :return:
+        """
+
+        self.title = title
+
+        if type(rel_year) == 'int':
+            self.rel_year = self.rel_year
+        else:
+            self.rel_year = int(rel_year)
+
+        #TODO correct
+        self.duration_mins = duration
+
+        self.director = director
+
+        self.actors = actors
+
+        self.rating = rating
+        self.voters = voters
+        self.ranking = ranking
+        self.orig_title = orig_title
+        self.genres = genres
+
+
+if __name__ == "__main__":
+    film = Film(title='Skazani na shawshank',
+                rel_year='1994')
+
+
+
+
+
+
--- project_imdb/imdb_manager/imdb_manager.py
+++ project_imdb/imdb_manager/imdb_manager.py
@@ -2,6 +2,7 @@ import pymysql
 from project_imdb.config import host, user, password, db
 from project_imdb.imdb_manager.imdb_queries import add_person_query, get_person_id_query, add_genre_query, \
     get_genre_id_query
+from project_imdb.imdb_manager.film import Film
 
 
 class Person:
@@ -31,9 +32,10 @@ class ImdbManager:
         """
         self.conn = pymysql.connect(host=host, user=user, password=password, charset='utf8', db=db)
 
-    def addPerson(self, person, table):
+    def _addPerson(self, person, table):
         """
-        adds person to  table given person object
+        adds person to  table given person object,
+        private method used by addActor and addDirector
         :param person: person object
         :param table: target table
         :return:
@@ -42,7 +44,7 @@ class ImdbManager:
         print(add_person_query % (table, person.first_name, person.last_name, person.nationality))
         cursor.execute(add_person_query % (table, person.first_name, person.last_name, person.nationality))
         self.conn.commit()
-        return self.getPersonId(person, table)
+        return self._getPersonId(person, table)
 
     def addActor(self, person):
         """
@@ -50,7 +52,7 @@ class ImdbManager:
         :param person: person object
         :return: id in actor table
         """
-        return self.addPerson(person, 'actor')
+        return self._addPerson(person, 'actor')
 
     def addDirector(self, person):
         """
@@ -58,9 +60,9 @@ class ImdbManager:
         :param person: person object
         :return: id in director table
         """
-        return self.addPerson(person, 'director')
+        return self._addPerson(person, 'director')
 
-    def getPersonId(self, person, table):
+    def _getPersonId(self, person, table):
         """
         gets person id in given table and given person object
         :param person: person object
@@ -80,9 +82,9 @@ class ImdbManager:
         cursor = self.conn.cursor()
         cursor.execute(add_genre_query % genre.name)
         self.conn.commit()
-        return self.getGenreId(genre)
+        return self._getGenreId(genre)
 
-    def getGenreId(self, genre):
+    def _getGenreId(self, genre):
         """
         gets id in genre table for genre object
         :param genre: genre object
@@ -91,6 +93,22 @@ class ImdbManager:
         cursor = self.conn.cursor()
         cursor.execute(get_genre_id_query % genre.name)
         return cursor.fetchall()[0][0]
+
+    def addFilm(self, film):
+        # check if film exists in table
+        # if not add row
+        pass
+
+    def _addFilmRow(self, film):
+        pass
+
+    def _addActorInFilm(self):
+        pass
+
+    def _addGenreInFilm(self):
+        pass
+
+
 
 
 if __name__ == "__main__":
